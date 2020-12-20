fb877d4a673bb1d007fc9096169d8e021a5d592d
aarora8 <aarora8@jhu.edu>
2020-12-20 00:31:49 -0500

    adding classify names

---
+++ examples/classify_names/run_classify.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# This script creates traing and validations splits, downloads text corpus for language modeling,
+#  prepares the training, validation and test data for rimes dataset 
+# (i.e text, images.scp, utt2spk and spk2utt). It calls process_data.py.
+#  Eg. local/prepare_data.sh
+
+stage=0
+download_dir=data/local/rimes_data
+train_img_url="https://dl.fbaipublicfiles.com/fairseq/data/tutorial_names.tar.gz"
+. ./cmd.sh
+. ./path.sh
+. ./utils/parse_options.sh || exit 1;
+
+if [ ${stage} -le 1 ]; then
+  wget -P $download_dir $train_img_url
+  tar -xzf $download_dir/tutorial_names.tar.gz
+
+  fairseq-preprocess \
+    --trainpref names/train --validpref names/valid --testpref names/test \
+    --source-lang input --target-lang label \
+    --destdir names-bin --dataset-impl raw
+fi
+
+if [ ${stage} -le 2 ]; then
+
+  fairseq-train names-bin \
+    --task simple_classification \
+    --arch pytorch_tutorial_rnn \
+    --optimizer adam --lr 0.001 --lr-shrink 0.5 \
+    --max-tokens 1000
+fi
