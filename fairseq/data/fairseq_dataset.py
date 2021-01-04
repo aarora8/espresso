5a0742cc302df425319ff3d1501a0c9afb2b11a6
aarora8 <aarora8@jhu.edu>
2021-01-04 13:33:02 +0530

    commit 2, adding same and diff in fairseq dataset

--- fairseq/data/fairseq_dataset.py
+++ fairseq/data/fairseq_dataset.py
@@ -7,6 +7,7 @@ import numpy as np
 import logging
 import torch.utils.data
 from fairseq.data import data_utils
+from collections import Counter
 
 logger = logging.getLogger(__name__)
 
@@ -229,6 +230,69 @@ class FairseqDataset(torch.utils.data.Dataset, EpochListening):
         print("the batch shape is {}".format(np.shape(batches)))
         print("the batch 1 is {}".format(batches[1]))
         sentence = input('\nInput: ')
+        return batches
+
+    def batch_by_size_utils_baseline_triplet(
+            self,
+            indices,
+            num_tokens_vec,
+            max_tokens,
+            max_sentences,
+            bsz_mult,
+    ):
+        """Simple, reliable and slow implementation of batch by size """
+        batches = []
+        start = 0
+        logger.info("enter batch by size utils baseline")
+        print(self.tgt_dict.values())
+        print(
+            "***************************************************************************************||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+        print('| {} examples/sentences'.format(len(indices)))
+        while start < len(indices):
+            for end in range(start + 1, len(indices) + 1):
+                max_val = max(num_tokens_vec[pos] for pos in range(start, end))
+                sent_count = end - start
+                num_tokens = max_val * sent_count
+                overflow = num_tokens > max_tokens > 0 or sent_count > max_sentences > 0
+                terminate = overflow or end == len(indices)
+                if overflow:
+                    sent_count -= 1
+                if terminate:
+                    if sent_count > bsz_mult:
+                        sent_count = sent_count - sent_count % bsz_mult
+                    batches.append(indices[start: start + sent_count])
+                    start = start + sent_count
+                    break
+
+        print("the number of batches are {}".format(len(batches)))
+        print("the batch shape is {}".format(np.shape(batches)))
+        print("the batch 1 is {}".format(batches[1]))
+        sentence = input('\nInput: ')
+
+        print("unique lables are {}".format(np.unique(self.tgt)))
+        print("lables at fifth location is {}".format(self.tgt[4].numpy()))
+        print("lables at fifth location is {}".format(self.tgt[253].numpy()))
+        tensor_to_np_labels = np.array(self.tgt)
+        id_counts = Counter(tensor_to_np_labels)
+        print(tensor_to_np_labels)
+        same = []
+        for index, word_id in enumerate(self.tgt):  # collect same samples
+            indices = np.where(tensor_to_np_labels == word_id.numpy())[0]
+            same.append(np.random.permutation(indices[indices != index])[:1])
+        # same = np.array(same)
+        # print("value same {}".format(same))
+        # print("shape same ", same.shape)
+
+
+        diff_ids = np.random.randint(0, len(self.tgt_dict) - 1, (len(self.tgt), 5))
+        diff_ids[diff_ids >= np.tile(tensor_to_np_labels.reshape(-1, 1), [1, 5])] += 1
+        diff = np.full_like(diff_ids, 0, dtype=np.int32)
+        for word_id, count in id_counts.items():  # collect diff samples
+            indices = np.where(diff_ids == word_id)
+            diff[indices] = np.where(tensor_to_np_labels == word_id)[0][np.random.randint(0, count, len(indices[0]))]
+            # for i in range(len(labels)):
+            #     print("value same {}".format(diff[i]))
+            # sentence = input('\nInput: ')
         return batches
 
     def filter_indices_by_size(self, indices, max_sizes):
