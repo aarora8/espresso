8cf96e888def8872c7d193b4b755c85bee4fa67b
aarora8 <aarora8@jhu.edu>
2021-01-30 22:55:24 +0530

    updating script to use decoding nstead of alignment

--- examples/classify_names/get_decoded_sausage.sh
+++ examples/classify_names/get_decoded_sausage.sh
@@ -19,7 +19,7 @@ mkdir -p $log
 nj=$(ls $lat_dir/lat.*.gz | wc -l)
 
 
-if [ $stage -le -6 ]; then
+if [ $stage -le -5 ]; then
   steps/nnet3/align_lats.sh --nj 32 --cmd "$train_cmd" \
     --acoustic-scale 1.0 \
     --scale-opts '--transition-scale=1.0 --self-loop-scale=1.0' \
@@ -27,11 +27,11 @@ if [ $stage -le -6 ]; then
     data/train_sp_hires data/lang_nosp_test exp/chain_a/tdnn_a_spec exp/lats
 fi
 
-if [ $stage -le -5 ]; then
+if [ $stage -le -4 ]; then
   utils/mkgraph.sh --self-loop-scale 1.0 data/lang_nosp_test exp/chain_a/tdnn_a_spec exp/chain_a/tdnn_a_spec/graph
 fi
 
-if [ $stage -le -4 ]; then
+if [ $stage -le -3 ]; then
   steps/nnet3/decode.sh --num-threads 4 --nj 20 --cmd "$decode_cmd" \
       --acwt 1.0 --post-decode-acwt 10.0 \
       --online-ivector-dir exp/nnet3_a/ivectors_train_sp_hires \
@@ -39,7 +39,7 @@ if [ $stage -le -4 ]; then
 fi
 
 
-if [ $stage -le -3 ]; then
+if [ $stage -le -2 ]; then
   steps/nnet3/align.sh --nj 32 --cmd "$train_cmd" \
       --use-gpu true \
       --scale-opts '--transition-scale=1.0 --self-loop-scale=1.0 --acoustic-scale=1.0' \
@@ -47,19 +47,14 @@ if [ $stage -le -3 ]; then
       data/train_sp_hires data/lang_nosp_test exp/chain_a/tdnn_a_spec/ exp/chain_a/tdnn_a_spec/align_train_sp
 fi
 
-if [ $stage -le -2 ]; then
-  for i in $(seq 1 $nj);
-  do
-      ali-to-phones --ctm-output exp/chain_a/tdnn_a_spec/final.mdl ark:"gunzip -c exp/chain_a/tdnn_a_spec/align_train_sp/ali.${i}.gz|" exp/chain_a/tdnn_a_spec/align_train_sp/${prefix}_${i}.ctm.int
-      cat exp/chain_a/tdnn_a_spec/align_train_sp/${prefix}_${i}.ctm.int | int2sym.pl -f 5 data/lang_nosp_test/phones.txt > exp/chain_a/tdnn_a_spec/align_train_sp/${prefix}_${i}.ctm.words
-  done
-fi
-
 if [ $stage -le -1 ]; then
-  awk '{print $1" "$1" 1"}' data/train_sp_hires/wav.scp > data/train_sp_hires/reco2file_and_channel
+#  for i in $(seq 1 $nj);
+#  do
+#      ali-to-phones --ctm-output exp/chain_a/tdnn_a_spec/final.mdl ark:"gunzip -c exp/chain_a/tdnn_a_spec/align_train_sp/ali.${i}.gz|" exp/chain_a/tdnn_a_spec/align_train_sp/${prefix}_${i}.ctm.int
+#      cat exp/chain_a/tdnn_a_spec/align_train_sp/${prefix}_${i}.ctm.int | int2sym.pl -f 5 data/lang_nosp_test/phones.txt > exp/chain_a/tdnn_a_spec/align_train_sp/${prefix}_${i}.ctm.words
+#  done
   steps/get_train_ctm.sh data/train_sp_hires/ data/lang_nosp_test/ exp/chain_a/tdnn_a_spec/align_train_sp/ exp/chain_a/tdnn_a_spec/align_train_sp/
 fi
-
 exit
 if [ $stage -le 0 ] ; then
   for i in $(seq 1 $nj);
@@ -67,6 +62,7 @@ if [ $stage -le 0 ] ; then
       lattice-align-words $lang/phones/word_boundary.int $model \
           "ark:gunzip -c $lat_dir/lat.${i}.gz|" ark:- | \
           lattice-to-phone-lattice $model ark:- ark:- | \
+          lattice-align-phones
           lattice-mbr-decode ark:- ark,t:$log/${prefix}_${i}.text  ark:/dev/null ark,t:$log/${prefix}_${i}.sau ark,t:$log/${prefix}_${i}.time
   done
 fi
---
+++ examples/classify_names/get_sausage_segment4.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+from collections import namedtuple
+
+
+utt_sau_dict = dict()
+phones_count_sau = dict()
+text_file = os.path.join("/Users/ashisharora/data_prep_siamese/", 'train.sau')
+text_fh = open(text_file, 'r', encoding='utf-8')
+for line in text_fh:
+    uttid = line.split(" ")[0]
+    sauasge_transcription = list()
+    line_wo_uttid = " ".join(line.strip().split(" ")[1:])
+    line_wo_uttid = line_wo_uttid.strip().split("[ 0 ")
+    for element in line_wo_uttid:
+        count = element.count("[")
+        if count != 0:
+            start_index = element.index("[")
+            element = element[start_index:]
+            sauasge_transcription.append(element)
+    utt_sau_dict[uttid] = sauasge_transcription
+    phones_count_sau[uttid] = len(sauasge_transcription)
+
+
+reco_sau_dict = dict()
+for uttid in sorted(utt_sau_dict.keys()):
+    recoid = "_".join(uttid.strip().split("_")[3:])
+    if recoid not in list(reco_sau_dict.keys()):
+        reco_sau_dict[recoid] = list()
+    for sausage_element in utt_sau_dict[uttid]:
+        reco_sau_dict[recoid].append(sausage_element)
+
+
+start_end_set = namedtuple('start_end_set', 'uttid start end')
+utt_time_dict = dict()
+utt_time_elements_count_dict = dict()
+text_file = os.path.join("/Users/ashisharora/data_prep_siamese/", 'train.time')
+text_fh = open(text_file, 'r', encoding='utf-8')
+for line in text_fh:
+    uttid = line.split(" ")[0]
+    count = line.count(";")
+    line_wo_uttid = " ".join(line.strip().split(" ")[1:])
+    start_end_list = line_wo_uttid.strip().split(";")
+    utt_time_list = list()
+    for element in start_end_list:
+        element = element.strip()
+        utt_time_list.append(start_end_set(
+        uttid = uttid,
+        start = element.split(" ")[0],
+        end = element.split(" ")[1]
+    ))
+    utt_time_dict[uttid] = utt_time_list
+    utt_time_elements_count_dict[uttid] = len(utt_time_list)
+
+reco_start_end_set = namedtuple('reco_start_end_set', 'uttid start end')
+reco_time_dict = dict()
+for uttid in sorted(utt_time_dict.keys()):
+    recoid = "_".join(uttid.strip().split("_")[3:])
+    utt_start_frame = float(uttid.strip().split("_")[1])/3
+    if recoid not in list(reco_time_dict.keys()):
+        reco_time_dict[recoid] = list()
+    for start_end_set in utt_time_dict[uttid]:
+        start = float(start_end_set.start) + utt_start_frame
+        end = float(start_end_set.end) + utt_start_frame
+        reco_time_dict[recoid].append(reco_start_end_set(
+            uttid = uttid,
+            start = start,
+            end = end
+        ))
+
+Segment = namedtuple('Segment', 'recoid word start end')
+utt_stend_time_ctm_dict = dict()
+text_file = os.path.join("/Users/ashisharora/data_prep_siamese/align_train_sp", 'ctm')
+text_fh = open(text_file, 'r', encoding='utf-8')
+for line in text_fh:
+    segment_fields = line.strip().split()
+    if "sp0.9" in segment_fields[0]:
+        continue
+    if "sp1.1" in segment_fields[0]:
+        continue
+    start = float(segment_fields[2])
+    start_frame = (start * 100) / 3
+    duration = float(segment_fields[3])
+    end = start + duration
+    end_frame = (end * 100) / 3
+    if segment_fields[0] not in list(utt_stend_time_ctm_dict.keys()):
+        utt_stend_time_ctm_dict[segment_fields[0]] = list()
+    utt_stend_time_ctm_dict[segment_fields[0]].append(Segment(
+        recoid = segment_fields[0],
+        word = segment_fields[4],
+        start = start_frame,
+        end = end_frame
+    ))
+
+
+train_text_handle = open("/Users/ashisharora/data_prep_siamese/train.input-label.input.2", 'w', encoding='utf8')
+train_text_handle2 = open("/Users/ashisharora/data_prep_siamese/train.input-label.label.2", 'w', encoding='utf8')
+uttword_sauseg_dict = dict()
+for recoid in sorted(utt_stend_time_ctm_dict.keys()):
+    for Segment in utt_stend_time_ctm_dict[recoid]:
+        word = Segment.word
+        start_frame = Segment.start
+        end_frame = Segment.end
+        if word == "<UNK>":
+            continue
+        sauasge_list = reco_sau_dict[recoid]
+        time_list = reco_time_dict[recoid]
+        assert len(sauasge_list) == len(time_list)
+        count_start = 0
+        for time in time_list:
+            start_time = time.start
+            if start_time >= start_frame:
+                break
+            count_start += 1
+        count_end = 0
+        for time in time_list:
+            count_end += 1
+            end_time = time.end
+            if end_time >= end_frame:
+                break
+        train_text_handle.write(' '.join(sauasge_list[count_start:(count_start + count_end)]) + '\n')
+        train_text_handle2.write(word + '\n')
